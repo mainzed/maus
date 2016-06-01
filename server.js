fe14a200d5966f6e93ad33044452607c9ad726d0
SHANYUAN <shanyuan55@icloud.com>
2016-06-01 17:45:00 +0200

    minor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000; // changes11
+var port = process.env.PORT || 3000; // changes1
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 //var mongoose = require('mongoose');
