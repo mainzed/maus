cee5ffec9070ce42687f8dabd642cb948238a740
MatthiasDfnr <matthias.dufner@hs-mainz.de>
2016-06-15 15:28:41 +0200

    new font

--- app/preview_files/opmainzed/javascript/app.js
+++ app/preview_files/opmainzed/javascript/app.js
@@ -4,17 +4,17 @@ var is_iPad = navigator.userAgent.match(/iPad/i) != null;
 var mobile = false;
 
 $(document).ready(function() {
-
+    
     // initScroller();
     checkBrowserWidth();
-    //console.log( $(window).scrollTop());
-
+    console.log( $(window).scrollTop());
+        
 
     // initialize menu
     if($(window).scrollTop() == 0){
         if(mobile){
             $("#ressources").addClass("startanimationm");
-        }
+        } 
         else {
             $("#ressources").addClass("startanimation");
         }
@@ -27,7 +27,7 @@ $(document).ready(function() {
     // Ipad Horizontal Background Click Fix
     if(is_iPad){$("*").css("cursor", "pointer");}
 
-
+        
 
 
     // Waypoints - Ein- und Ausblenden von Ressourcen
@@ -40,7 +40,7 @@ $(document).ready(function() {
                 showTitle();
             }
         }
-    })
+    })    
 
 
     // animierter Ankerlink
@@ -52,19 +52,19 @@ $(document).ready(function() {
         return false;
     });
     */
-
-
+  
+ 
 
 }); //close ready
 
     $( window ).resize(function() {
-        checkBrowserWidth();
-        console.log(mobile);
+        checkBrowserWidth(); 
+        console.log(mobile); 
         clearRessource();
     });
 
 
-   $("a").click(function(event){
+   $("a").click(function(event){     
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
     });
@@ -72,8 +72,8 @@ $(document).ready(function() {
 
 
 
-
-
+    
+    
 
     $("#ressources").click(function(e){
         e.stopPropagation();
@@ -110,7 +110,7 @@ $(document).ready(function() {
         e.stopPropagation();
     });
 
-
+    
     $('.picture').click(function(e){
         zoomPicture($(this));
         e.stopPropagation();
@@ -125,8 +125,8 @@ function checkBrowserWidth(){
     else {
         mobile = false;
     }
-}
-
+}    
+    
 
 
 function showTitle(){
@@ -134,12 +134,12 @@ function showTitle(){
    clearRessource();
     if(mobile){
         $("#ressources").addClass("startanimationm");
-    }
+    } 
     else {
         $("#ressources").addClass("startanimation");
     }
     $("#titletextbg").show();
-
+   
 }
 
 
@@ -147,7 +147,7 @@ function showTitle(){
 
 
 function clearRessource(){
-
+    
     // remove classes
     $("#ressources").removeClass();
     $("#navicon").removeClass();
@@ -159,15 +159,16 @@ function clearRessource(){
     $("#ressources img").remove();
     $(".figcaption").remove();
     $("#titletextbg").hide();
+    $("#imagecontainer").hide();  
 
     // hide navigation elements
     $("#nav").hide();
     $("#closeicon").hide();
-
+    
 
     if(mobile){
         $("#ressources").addClass("minifiedm");
-    }
+    } 
     else {
         $("#ressources").addClass("minified");
     }
@@ -175,13 +176,14 @@ function clearRessource(){
 }
 
 function resetRessource(navicon, nav, closeicon){
-
+    
     // remove classes
     $("#ressources").removeClass();
     $("#navicon").removeClass();
     $("#closeicon").removeClass();
     $(".activeressource").removeClass("activeressource");
     $("#titletextbg").hide();
+    $("#imagecontainer").hide();  
 
     // clear content
     $("#ressourcestext").text("");
@@ -211,11 +213,11 @@ function resetRessource(navicon, nav, closeicon){
 
 
 function showTableOfContent(){
-
+     
     resetRessource(false, true, true);
     if(mobile){
         $("#ressources").addClass("showtableofcontent");
-    }
+    } 
     else {
         $("#ressources").addClass("showdefinitions");
     }
@@ -223,21 +225,21 @@ function showTableOfContent(){
     // set active layer
     $(".activeressource").removeClass("activeressource");
     $("#nav").addClass("activeressource");
-
+  
 }
 
 function showGlossar(clickedword){
-
+    
     resetRessource(false, false, true);
 
     // find tooltip text
-    var identifier = clickedword.attr('id');
+    var identifier = clickedword.attr('id');            
     $("." + identifier).clone().appendTo("#ressourcestext");
-
-
+    
+    
     if(mobile){
         $("#ressources").addClass("showdefinitionsm");
-    }
+    } 
     else {
         $("#ressources").addClass("showdefinitions");
     }
@@ -245,9 +247,9 @@ function showGlossar(clickedword){
     // set active layer
     $(".activeressource").removeClass("activeressource");
     $("#ressourcestext").addClass("activeressource");
-
-
-
+              
+    
+    
     if(mobile==false){
         $("#navicon").show();
     } else {
@@ -258,22 +260,24 @@ function showGlossar(clickedword){
 function zoomPicture(clickedpicture){
     if(!mobile){
         resetRessource(true, false, true);
-        var source = clickedpicture.attr('src');
-        var figcaption = $('<p class="figcaption">' + clickedpicture.next("figcaption").text() + '</p>');
-        var zoomedpicture = $("<img src='" + source + "' />");
-        zoomedpicture.appendTo("#ressources");
-        figcaption.appendTo("#ressources");
+        var source = clickedpicture.attr('src');     
+        var figcaption = $('<p class="figcaption">' + clickedpicture.next("figcaption").text() + '</p>');      
+        //var zoomedpicture = $("<img src='" + source + "' />");        
+        //zoomedpicture.appendTo("#ressources");
+        figcaption.appendTo("#ressources"); 
+        $("#imagecontainer").css("background", "url(" + source + ") black");
+        $("#imagecontainer").show();  
 
         if(mobile){
             $("#ressources").addClass("showpicturesm");
-        }
+        } 
         else {
             $("#ressources").addClass("showpictures");
-        }
-
+        }   
+                   
         $("#navicon").addClass("white");
         $("#closeicon").addClass("white");
-
+       
     }
     else {
         clearRessource();
@@ -287,4 +291,9 @@ function initScroller() {
                     anchor[i].onclick = Scroller;
                 }
             }
-*/
+*/            
+
+
+
+
+
