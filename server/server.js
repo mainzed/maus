07bdd5835abe77d4b50aa78b2454e34e2f1f6847
Axel Kunz <shanyuan55@icloud.com>
2017-07-17 09:53:40 +0200

    fixed paylood too large on large preview request

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ var cors = require("cors");
 var api = require("./routes/api");
 var authenticate = require("./routes/authenticate")(passport);
 
+// set file sizes
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 // middleware
 app.use(cors());
 app.use(compression());  // compress static content using gzip
