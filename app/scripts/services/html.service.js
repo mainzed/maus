9b191ab7a765b89ddf327aa0acc1d76c79c86c9e
Axel Kunz <shanyuan55@icloud.com>
2017-07-21 15:00:43 +0200

    for annual reports, replaced src with data-original attribute for images to work with the jQuery lazyload plugin

--- app/scripts/services/html.service.js
+++ app/scripts/services/html.service.js
@@ -128,44 +128,45 @@ angular.module('meanMarkdownApp')
     };
 
     /**
-     * uses the marked function to convert markdown to html
+     * Convert markdown to jahresbericht HTML by using a customized marked
+     * renderer
      */
-    this.convertOpMainzedMarkdownToHTML = function(markdown) {
-        var customRenderer = new marked.Renderer();
+    this.convertOpMainzedMarkdownToHTML = function (markdown) {
+      var customRenderer = new marked.Renderer()
 
-        var h1Counter = 0;
-        var h2Counter = 0;
-        var h3Counter = 0;
-        customRenderer.heading = function (text, level) {
+      var h1Counter = 0
+      var h2Counter = 0
+      var h3Counter = 0
+      customRenderer.heading = function (text, level) {
+        if (level === 1) {
+          h1Counter++
+          h2Counter = 0
+          h3Counter = 0
+          return '<h1 id="section-' + h1Counter + '" class="hyphenate">' + text + '</h1>\n'
+        } else if (level === 2) {
+          h2Counter++
+          h3Counter = 0
+          return '<h2 id="section-' + h1Counter + '-' + h2Counter + '" class="hyphenate">' + text + '</h2>\n'
+        } else if (level === 3) {
+          h3Counter++
+          return '<h3 id="section-' + h1Counter + '-' + h2Counter + '-' + h3Counter + '" class="hyphenate">' + text + '</h3>\n'
+        }
+      }
 
-            if (level === 1) {
-                h1Counter++;
-                h2Counter = 0;
-                h3Counter = 0;
+      customRenderer.link = function (linkUrl, noIdea, text) {
+        if (linkUrl.startsWith('#')) {   // internal link
+          return '<a href="' + linkUrl + '" class="internal-link">' + text + '</a>'
+        } else {  // external links
+          return '<a href="' + linkUrl + '" class="external-link" target="_blank">' + text + '</a>'
+        }
+      }
 
-                return '<h1 id="section-' + h1Counter + '" class="hyphenate">' + text + '</h1>\n';
-            } else if (level === 2) {
-                h2Counter++;
-                h3Counter = 0;
-                return '<h2 id="section-' + h1Counter + "-" + h2Counter + '" class="hyphenate">' + text + '</h2>\n';
-
-            } else if (level === 3) {
-                h3Counter++;
-                return '<h3 id="section-' + h1Counter + "-" + h2Counter + "-" + h3Counter + '" class="hyphenate">' + text + '</h3>\n';
-            }
-        };
-
-        // custom link renderer
-        customRenderer.link = function (linkUrl, noIdea, text) {
-            if (linkUrl.startsWith("#")) {   // internal link
-                return "<a href=\"" + linkUrl + "\" class='internal-link'>" + text + "</a>";
-            } else {  // external links
-                return "<a href=\"" + linkUrl + "\" class='external-link' target=\"_blank\">" + text + "</a>";
-            }
-
-        };
-        return marked(markdown, { renderer: customRenderer });
-    };
+      // use data-original instead of src to work with the jQuery lazyload plugin
+      customRenderer.image = function (src, title, alt) {
+        return '<img data-original="' + src + '" alt="' + alt + '">'
+      }
+      return marked(markdown, { renderer: customRenderer })
+    }
 
     /**
      * uses the marked function to convert markdown to html
@@ -370,7 +371,7 @@ angular.module('meanMarkdownApp')
 
         figureString = [
             '<figure id="' + enrichment._id + '">',
-                '<img src="' + enrichment.url + '" class="picture" alt="' + enrichment.title + '">',
+                '<img data-original="' + enrichment.url + '" class="picture" alt="' + enrichment.title + '">',
                 '<figcaption>',
                     enrichment.text,
                     metadataString,
